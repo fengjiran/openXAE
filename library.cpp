c4f627821f0b30843e6fc3aad239d90b2c64a22c
fengjiran <fengjiran@foxmail.com>
2023-06-12 23:14:48 +0800

    add tvm header files

--- library.cpp
+++ library.cpp
@@ -1,6 +1,17 @@
-#include "library.h"
-
 #include <iostream>
+#include "library.h"
+#include "tvm/runtime/object.h"
+#include "tvm/runtime/registry.h"
+
+using TVMArgs = tvm::runtime::TVMArgs;
+using TVMRetValue = tvm::runtime::TVMRetValue;
+using PackedFunc = tvm::runtime::PackedFunc;
+
+void ToyAdd(TVMArgs args, TVMRetValue* rv) {
+    // automatically convert arguments to desired type.
+    int a = args[0];
+    int b = args[1];
+}
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
