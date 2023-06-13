a670989f70d23f9880bfc91421f46d60b8fde904
fengjiran <fengjiran@foxmail.com>
2023-06-13 21:19:48 +0800

    add toy_sub packed func

--- include/library.h
+++ include/library.h
@@ -8,8 +8,15 @@ inline T toy_add(T a, T b) {
     return a + b;
 }
 
+template<typename T>
+inline T toy_sub(T a, T b) {
+    return a - b;
+}
+
 void ListGlobalFuncNames();
 
 void test_toy_add(float a, float b);
+
+void test_toy_sub(float a, float b);
 
 #endif //OPENXAE_LIBRARY_H
--- library.cpp
+++ library.cpp
@@ -5,12 +5,27 @@
 using namespace tvm;
 using namespace tvm::runtime;
 
-TVM_REGISTER_GLOBAL("toy_add").set_body([](TVMArgs args, TVMRetValue *rv) -> void {
+TVM_REGISTER_GLOBAL("toy_add").set_body([](TVMArgs args, TVMRetValue* rv) -> void {
     *rv = toy_add<double>(args[0], args[1]);
+});
+
+TVM_REGISTER_GLOBAL("toy_sub").set_body([](TVMArgs args, TVMRetValue* rv) -> void {
+    *rv = toy_sub<double>(args[0], args[1]);
 });
 
 void test_toy_add(float a, float b) {
     String fname = "toy_add";
+    const PackedFunc* fp = Registry::Get(fname);
+    if (fp != nullptr) {
+        double res = (*fp)(a, b);
+        LOG_INFO << "Result: " << res;
+    } else {
+        LOG_ERROR << "No PackedFunc: " << fname;
+    }
+}
+
+void test_toy_sub(float a, float b) {
+    String fname = "toy_sub";
     const PackedFunc* fp = Registry::Get(fname);
     if (fp != nullptr) {
         double res = (*fp)(a, b);
