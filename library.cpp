881116c1b6bc0d2bb3b0c2d8e9b9cf4166513afb
fengjiran <fengjiran@foxmail.com>
2023-06-13 21:44:32 +0800

    add toy_sub packed func

--- library.cpp
+++ library.cpp
@@ -5,17 +5,17 @@
 using namespace tvm;
 using namespace tvm::runtime;
 
-TVM_REGISTER_GLOBAL("toy_add").set_body([](TVMArgs args, TVMRetValue* rv) -> void {
+TVM_REGISTER_GLOBAL("toy_add").set_body([](TVMArgs args, TVMRetValue *rv) -> void {
     *rv = toy_add<double>(args[0], args[1]);
 });
 
-TVM_REGISTER_GLOBAL("toy_sub").set_body([](TVMArgs args, TVMRetValue* rv) -> void {
+TVM_REGISTER_GLOBAL("toy_sub").set_body([](TVMArgs args, TVMRetValue *rv) -> void {
     *rv = toy_sub<double>(args[0], args[1]);
 });
 
 void test_toy_add(float a, float b) {
     String fname = "toy_add";
-    const PackedFunc* fp = Registry::Get(fname);
+    const PackedFunc *fp = Registry::Get(fname);
     if (fp != nullptr) {
         double res = (*fp)(a, b);
         LOG_INFO << "Result: " << res;
@@ -26,7 +26,7 @@ void test_toy_add(float a, float b) {
 
 void test_toy_sub(float a, float b) {
     String fname = "toy_sub";
-    const PackedFunc* fp = Registry::Get(fname);
+    const PackedFunc *fp = Registry::Get(fname);
     if (fp != nullptr) {
         double res = (*fp)(a, b);
         LOG_INFO << "Result: " << res;
@@ -37,7 +37,7 @@ void test_toy_sub(float a, float b) {
 
 void ListGlobalFuncNames() {
     int global_func_num;
-    const char** plist;
+    const char **plist;
     TVMFuncListGlobalNames(&global_func_num, &plist);
 
     LOG_INFO << "List all " << global_func_num << " global functions:";
