b4087c6d30cef9b47ebac262f5ed2705583ab8a4
fengjiran <fengjiran@foxmail.com>
2023-06-14 16:50:41 +0800

    check PackedFunc point is or not null

--- library.cpp
+++ library.cpp
@@ -16,23 +16,17 @@ TVM_REGISTER_GLOBAL("toy_sub").set_body([](TVMArgs args, TVMRetValue *rv) -> voi
 void test_toy_add(float a, float b) {
     String fname = "toy_add";
     const PackedFunc *fp = Registry::Get(fname);
-    if (fp != nullptr) {
-        double res = (*fp)(a, b);
-        LOG_INFO << "Result: " << res;
-    } else {
-        LOG_ERROR << "No PackedFunc: " << fname;
-    }
+    ICHECK(fp != nullptr);
+    double res = (*fp)(a, b);
+    LOG_INFO << "Result: " << res;
 }
 
 void test_toy_sub(float a, float b) {
     String fname = "toy_sub";
     const PackedFunc *fp = Registry::Get(fname);
-    if (fp != nullptr) {
-        double res = (*fp)(a, b);
-        LOG_INFO << "Result: " << res;
-    } else {
-        LOG_ERROR << "No PackedFunc: " << fname;
-    }
+    ICHECK(fp != nullptr);
+    double res = (*fp)(a, b);
+    LOG_INFO << "Result: " << res;
 }
 
 void ListGlobalFuncNames() {
