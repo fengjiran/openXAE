75ef0a28a2a5ada652aadde036aca0e19038131c
fengjiran <fengjiran@foxmail.com>
2023-06-15 19:22:38 +0800

    format

--- include/library.h
+++ include/library.h
@@ -1,15 +1,15 @@
 #ifndef OPENXAE_LIBRARY_H
 #define OPENXAE_LIBRARY_H
 
-//#define TVM_DLL __attribute__((visibility("default")))
-
-template<typename T>
-inline T toy_add(T a, T b) {
+template <typename T>
+inline T toy_add(T a, T b)
+{
     return a + b;
 }
 
-template<typename T>
-inline T toy_sub(T a, T b) {
+template <typename T>
+inline T toy_sub(T a, T b)
+{
     return a - b;
 }
 
@@ -20,4 +20,4 @@ void test_toy_add(float a, float b);
 void test_toy_sub(float a, float b);
 
 void ListTypeTable();
-#endif //OPENXAE_LIBRARY_H
+#endif // OPENXAE_LIBRARY_H
