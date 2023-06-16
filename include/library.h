4304f4aea6086e0784dade69604d0f105de13bf9
fengjiran <fengjiran@foxmail.com>
2023-06-16 08:50:38 +0800

    format the code

--- include/library.h
+++ include/library.h
@@ -1,15 +1,13 @@
 #ifndef OPENXAE_LIBRARY_H
 #define OPENXAE_LIBRARY_H
 
-template <typename T>
-inline T toy_add(T a, T b)
-{
+template<typename T>
+inline T toy_add(T a, T b) {
     return a + b;
 }
 
-template <typename T>
-inline T toy_sub(T a, T b)
-{
+template<typename T>
+inline T toy_sub(T a, T b) {
     return a - b;
 }
 
@@ -20,4 +18,4 @@ void test_toy_add(float a, float b);
 void test_toy_sub(float a, float b);
 
 void ListTypeTable();
-#endif // OPENXAE_LIBRARY_H
+#endif// OPENXAE_LIBRARY_H
