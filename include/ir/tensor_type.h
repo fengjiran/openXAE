174fe1c4bf942d8547c213b78280d8f62a59c631
fengjiran <fengjiran@foxmail.com>
2023-06-20 16:27:56 +0800

    add tensor type

---
+++ include/ir/tensor_type.h
@@ -0,0 +1,8 @@
+//
+// Created by richard on 6/20/23.
+//
+
+#ifndef OPENXAE_TENSOR_TYPE_H
+#define OPENXAE_TENSOR_TYPE_H
+
+#endif//OPENXAE_TENSOR_TYPE_H
---
+++ src/ir/tensor_type.cpp
@@ -0,0 +1,5 @@
+//
+// Created by richard on 6/20/23.
+//
+
+#include "ir/tensor_type.h"
