8c7437d566fce623fea8c6a6b4b0212e29e686ce
fengjiran <fengjiran@foxmail.com>
2024-05-11 01:23:59 +0800

    test

---
+++ include/MyAllocator.hpp
@@ -0,0 +1,13 @@
+//
+// Created by 赵丹 on 24-5-10.
+//
+
+#ifndef OPENXAE_MYALLOCATOR_HPP
+#define OPENXAE_MYALLOCATOR_HPP
+
+namespace XAcceleratorEngine {
+
+
+}
+
+#endif//OPENXAE_MYALLOCATOR_HPP
---
+++ test/custom_allocator_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 赵丹 on 24-5-10.
+//
+
+#include "MyAllocator.hpp"
