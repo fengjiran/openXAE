4ae9e90de066ac063e4731f054b0699df85c0799
fengjiran <fengjiran@foxmail.com>
2024-05-15 17:39:42 +0800

    add get function for Attribute class

--- include/Attribute.hpp
+++ include/Attribute.hpp
@@ -6,6 +6,8 @@
 #define OPENXAE_ATTRIBUTE_HPP
 
 #include "Datatype.hpp"
+#include "glog/logging.h"
+#include <memory>
 #include <vector>
 
 namespace XAcceleratorEngine {
@@ -43,7 +45,51 @@ public:
      * Such as float32, int8, etc.
      */
     DataType type = DataType::kTypeUnknown;
+
+    /**
+     * @brief Gets the attribute data as a typed array.
+     * The attribute data is cleared after get by default.
+     *
+     * @tparam T Datatype to return (float, int, etc)
+     * @param clearWeight Whether to clear data after get
+     * @return Vector containing the attribute data
+     */
+    template<typename T>
+    std::vector<T> get(bool clearWeight = true);
 };
+
+template<typename T>
+std::vector<T> Attribute::get(bool clearWeight) {
+    CHECK(!weight.empty());
+    CHECK(type != DataType::kTypeUnknown);
+
+    const auto elemSize = sizeof(T);
+    CHECK(weight.size() % elemSize == 0);
+    const auto elemCnt = weight.size() / elemSize;
+
+    std::vector<T> tmp;
+    tmp.reserve(elemCnt);
+    switch (type) {
+        case DataType::kTypeFloat32: {
+            static_assert(std::is_same<T, float>::value);
+            auto* weightPtr = reinterpret_cast<float*>(weight.data());
+            for (size_t i = 0; i < elemCnt; ++i) {
+                float elem = *(weightPtr + i);
+                tmp.emplace_back(elem);
+            }
+            break;
+        }
+        default: {
+            LOG(FATAL) << "Unknown weight data type: " << int32_t(type);
+        }
+    }
+
+    if (clearWeight) {
+        weight.clear();
+    }
+
+    return tmp;
+}
 
 }// namespace XAcceleratorEngine
 
