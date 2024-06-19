89f0703f2ef4a35c36b754f2a5bb46580fd9c671
fengjiran <fengjiran@foxmail.com>
2024-06-19 20:54:57 +0800

    inline block

---
+++ tools/pnnx/tests/test_inline_block.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def myadd(self, x):
+        pass
+
+
+if __name__ == '__main__':
+    pass
